0db03eedf541906f78e14b131bc429fe9064b83f
raphaelmello <raphael.mello@fpf.br>
2023-11-21 16:13:27 -0400

    first commit

---
+++ bin/dart_functions_and_collections.dart
@@ -0,0 +1,5 @@
+import 'package:dart_functions_and_collections/dart_functions_and_collections.dart' as dart_functions_and_collections;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_functions_and_collections.calculate()}!');
+}
---
+++ test/dart_functions_and_collections_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_functions_and_collections/dart_functions_and_collections.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
